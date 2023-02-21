e9ae073894830cfad15ccf29e2230effc07726b2
sudoskys <coldlando@hotmail.com>
2023-02-21 20:41:11 +0800

    自动权重排序

--- src/llm_kira/client/Optimizer.py
+++ src/llm_kira/client/Optimizer.py
@@ -203,7 +203,7 @@ class SinglePoint(Point):
         # Knowledge Search
         # interaction 遗忘函数
         for item in knowledge:
-            item.weight.append(45)
+            item.weight.append(60)
         # 追溯搜索
         # knowledge 相似度检索
         for item in knowledge:
