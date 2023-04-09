d700f408ae0e3a887af1fd14da179b131e37a3e5
sudoskys <coldlando@hotmail.com>
2023-04-09 17:29:25 +0800

    del memory manager from agent.py

--- src/llm_kira/client/agent.py
+++ src/llm_kira/client/agent.py
@@ -56,26 +56,3 @@ class Conversation(object):
             self.__usage += usage
 
 
-class MemoryManager(object):
-    def __init__(self,
-                 profile: Union[Conversation, int],
-                 area: str = ""
-                 ):
-        """
-        记忆管理器
-        :param profile: Conversation 类型，或者 conversation_id
-        :param area: 后缀，用于区分不同的记忆空间
-        """
-        if not isinstance(profile, int):
-            profile = profile.conversation_id
-        self._DataManager = MsgFlow(uid=f"{profile}{area}")
-
-    def reset_bucket(self):
-        return self._DataManager.forget()
-
-    def read_bucket(self) -> List[Interaction]:
-        return self._DataManager.read()
-
-    def save_bucket(self, message: List[Interaction], override: bool = True):
-        self._DataManager.save(interaction_flow=message, override=override)
-        return message
