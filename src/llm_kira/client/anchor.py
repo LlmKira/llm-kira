d9cffe3ec2566ba9c2d0dd6ec88fbf0cbba77a1d
sudoskys <coldlando@hotmail.com>
2023-04-09 21:31:50 +0800

    clean client

--- src/llm_kira/client/anchor.py
+++ src/llm_kira/client/anchor.py
@@ -24,6 +24,8 @@ class ChatBot(object):
                  ):
         """
         对话机器人代理端
+        :param profile: Conversation
+        :param llm_model: LlmBase 类型，大语言模型
         """
         self.profile = profile
         self.prompt = None
@@ -41,7 +43,7 @@ class ChatBot(object):
         """
         :param prompt: PromptEngine
         :param predict_tokens: 预测 Token 位
-        :param llm_param: 大语言模型参数
+        :param llm_param: 大语言模型对应的参数样式
         :param parse_reply: Callable[[list], str] 覆写解析方法
         :param clean_prompt:
         """
